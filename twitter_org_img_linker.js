1cb8ff42b6ab4011a35e205b06b0858b3bb45924
atos.ki166@gmail.com <atos.ki166@gmail.com>
2016-10-11 00:48:04 +0900

    リファクタリング2

--- twitter_org_img_linker.js
+++ twitter_org_img_linker.js
@@ -123,13 +123,16 @@ function createLinkText (src, cnt) {
     
     var target = src + ":orig";
     
-    var $span = $("<span></span>");
-    $span.css("margin-right", "1em").text(circleNumberList[cnt]);
+    var $span = $("<span></span>").css("margin-right", "1em")
     
     var $showIcon = '<svg style="vertical-align:middle;" fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"/><path d="M11.5 9C10.12 9 9 10.12 9 11.5s1.12 2.5 2.5 2.5 2.5-1.12 2.5-2.5S12.88 9 11.5 9zM20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-3.21 14.21l-2.91-2.91c-.69.44-1.51.7-2.39.7C9.01 16 7 13.99 7 11.5S9.01 7 11.5 7 16 9.01 16 11.5c0 .88-.26 1.69-.7 2.39l2.91 2.9-1.42 1.42z"/></svg>';
     
     var $showA = $("<a></a>");
-    $showA.addClass("show_org_image").attr("data-index", cnt).attr("data-href", target).attr("download", filename).css("margin-right", "0.5em").append($showIcon);
+    $showA.addClass("show_org_image").attr("data-index", cnt).attr("data-href", target).attr("download", filename).css({
+            "margin-right" : "0.5em",
+            "text-decoration" : "none",
+            "color" : "hsl (0, 0%, 0%)"
+        }).append(circleNumberList[cnt] + $showIcon);
     
     var $dom = $span.append($showA);
     return  $dom;
